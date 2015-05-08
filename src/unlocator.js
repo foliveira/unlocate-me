a9ed9141893708b52a37205cd3148228fcea30fa
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2015-05-08 21:36:38 +0100

    Unlocator code done

--- src/unlocator.js
+++ src/unlocator.js
@@ -4,19 +4,15 @@ var async = require('async')
 var Chance = require('chance')
 var request = require('request').defaults({jar: true})
 
-function _register(cb) {
+function _register(callback) {
   var chance = new Chance()
   var email = chance.email({domain: chance.domain()})
-  var password = chance.sentence({words: 2}).replace(/\s+/g, '')
   var form = {
     'email': email,
     '_email': email,
-    'pass': password,
-    'i_agree': 1/*,
-    '_qf_page-0_next': 'Next',
-    '_save_': 'page-0',
-    'product_id_page-0': '2-2'
-    */
+    'pass': chance.sentence({words: 2}).replace(/\s+/g, ''),
+    'i_agree': 1,
+    '_qf_page-0_next': chance.word()
   }
 
   async.waterfall([
@@ -28,20 +24,22 @@ function _register(cb) {
 
       request.post(options, cb)
     },
-    function(err, res, cb) {
+    function(res, body, cb) {
       request(res.headers.location, cb)
     },
-    function(err, res, body, cb) {
+    function(res, body, cb) {
       var apiKey = body.match(/http:\/\/unlo\.it\/(\w*)/)
+
       cb(null, apiKey[1])
     }
-  ], function(err, apikey) {
-    console.log(apikey)
-  })
-
+  ], callback)
 }
 
 function _update(apikey) {
+  if (apiKey == null) {
+    return
+  }
+
   request('http://unlo.it/' + apikey)
 }
 
